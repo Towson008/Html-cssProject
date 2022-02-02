1b7073422cc9a01d285d89426ad9bc4b80bdb002
Towson008 <dhammyshenko@gmail.com>
2022-02-02 12:02:30 +0100

    initial commit.

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("this is a trial call")
+
+function greet (){
+    console.log("Morning Mr Tosin")
+}
+
+greet()
+
+
+let mass = function (){
+    console.log("this is the reverse function ")
+}
+
+mass()
+
+
+let fruits = ['Berry', 'Apple', " Mango", "Grape", "Banana", "Orange"];
+
+// let fruit = fruits.map()
+
+console.log(fruits)
